fd8922e2804d0093d80ee9d6bf0f195b26f3dc7a
Kristof Buts <github@kristofbuts.be>
2018-04-12 16:48:24 +0200

    jumbo

--- views/home.php
+++ views/home.php
@@ -2,7 +2,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-light fixed-top bg-light">
 	<div class="container">
-		<a class="navbar-brand" href="/">
+		<a class="navbar-brand logo" href="/">
 			Opschepper
 		</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu" aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,7 +13,12 @@
 			<ul class="navbar-nav ml-auto">
 				<li class="nav-item">
 					<a href="#" class="nav-link">
-						Home
+						Hoe
+					</a>
+				</li>
+				<li class="nav-item">
+					<a href="#" class="nav-link">
+						Missie
 					</a>
 				</li>
 				<li class="nav-item">
@@ -33,12 +38,23 @@
 
 <div class="jumbotron" id="jumbo_main">
 	<div class="container">
-		<h1>Opschepper</h1>
-		<p class="lead">Uw ruit, onze zorg</p>
+		<h1 class="logo">Opschepper</h1>
+		<p><a href="#" class="btn btn-secondary btn-lg">Doe mee</a></p>
 	</div><!-- /container -->
 </div><!-- /jumbotron -->
 
-<div class="container">
-	<h1>Opschepper</h1>
+<div class="container info_col">
+	<h2>Hoe werkt het?</h2>
+	<div class="row">
+		<div class="col-md-4">
+			<h3>Stap 1</h3>
+		</div><!-- /col-md-4 -->
+		<div class="col-md-4">
+			<h3>Stap 2</h3>
+		</div><!-- /col-md-4 -->
+		<div class="col-md-4">
+			<h3>Stap 3</h3>
+		</div><!-- /col-md-4 -->
+	</div><!-- /row -->
 </div><!-- /container -->
 
