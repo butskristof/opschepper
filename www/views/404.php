129a6e748b8832f50b2cd18624b2d166b18bdfff
Kristof Buts <kr.buts@gmail.com>
2018-05-15 12:08:54 +0200

    Improved 404 page

--- www/views/404.php
+++ www/views/404.php
@@ -1,4 +1,14 @@
-<?php $this->layout('template', ['title' => 'Opschepper - Pagina niet gevonden', 'id' => '404', 'extracss' => '/static/css/pages.css']) ?>
+<?php $this->layout('template', ['title' => 'Opschepper - Pagina niet gevonden', 'id' => '404', 'extracss' => '/static/css/home.css']) ?>
+
+<!-- NAVIGATION -->
+
+<nav class="navbar navbar-expand-md navbar-light fixed-top bg-light">
+    <div class="container">
+        <a href="#" class="navbar-brand logo">
+            Opschepper
+        </a>
+    </div><!-- /container -->
+</nav>
 
 <div class="container">
 	<h1>Pagina niet gevonden</h1>
